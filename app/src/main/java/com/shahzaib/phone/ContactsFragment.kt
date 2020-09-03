0ee1e210f39d9444d212b92ede6eb9ce58c6f306
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2020-09-03 17:12:48 +0500

    Got the contacts Fragment Working with reading of contacts inside the fragment

--- app/src/main/java/com/shahzaib/phone/ContactsFragment.kt
+++ app/src/main/java/com/shahzaib/phone/ContactsFragment.kt
@@ -1,16 +1,67 @@
 package com.shahzaib.phone
 
+import android.content.Context
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.shahzaib.phone.adapters.ContactsListAdapter
+import com.shahzaib.phone.databinding.ContactsFragmentBinding
 
 class ContactsFragment: Fragment() {
+    private lateinit var binding: ContactsFragmentBinding
     private lateinit var contactListAdapter: RecyclerView.Adapter<*>
 
+    private var contactList: ArrayList<Contact> = arrayListOf()
+    private lateinit var mContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.contacts_fragment, container, false)
+        binding = ContactsFragmentBinding.inflate(layoutInflater)
+        readContacts()
+        contactListAdapter = ContactsListAdapter(contactList, mContext)
+
+        binding.contactRecyclerView.apply {
+            layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false)
+            adapter = contactListAdapter
+        }
+
+        return binding.root
+    }
+
+    private fun readContacts() {
+        Log.i("readContacts", "Reading Contacts")
+        val contentResolver = mContext.contentResolver
+        val nameCursor: Cursor? = contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null, null, null, null)
+        if (nameCursor!!.moveToFirst()) {
+            do {
+                val id: String = nameCursor.getString(nameCursor.getColumnIndex(ContactsContract.Contacts._ID))
+                val name: String = nameCursor.getString(nameCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                var number: String = ""
+                if (nameCursor.getInt(nameCursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0){
+                    val numberCursor: Cursor? = contentResolver.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", arrayOf(id), null)
+                    while (numberCursor!!.moveToNext()) {
+                        number = numberCursor.getString(numberCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    }
+                    numberCursor.close()
+                }
+                contactList.add(Contact(name, number))
+            } while (nameCursor.moveToNext())
+        }
+        nameCursor.close()
     }
 }
--- app/src/main/java/com/shahzaib/phone/ViewPagerActivity.kt
+++ app/src/main/java/com/shahzaib/phone/ViewPagerActivity.kt
@@ -2,10 +2,7 @@ package com.shahzaib.phone
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -17,8 +14,6 @@ class ViewPagerActivity : AppCompatActivity() {
     private lateinit var mAdapter: ViewPagerAdapter
     private lateinit var tabLayoutMediator: TabLayoutMediator
 
-    private var contactList: ArrayList<Contact> = arrayListOf()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ViewPagerActivityBinding.inflate(layoutInflater)
@@ -27,10 +22,7 @@ class ViewPagerActivity : AppCompatActivity() {
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), CONTACTS_REQUEST_CODE)
-        else
-            readContacts()
 
-        Log.i("Contacts", contactList.toString())
         mAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
 
         tabLayoutMediator = TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
@@ -40,7 +32,7 @@ class ViewPagerActivity : AppCompatActivity() {
             }
             else if (position == 1){
                 tab.text = "Contacts"
-                mAdapter.createFragment(position, contactList)
+                mAdapter.createFragment(position)
             }
         }
 
@@ -50,32 +42,6 @@ class ViewPagerActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CONTACTS_REQUEST_CODE)
-            readContacts()
-    }
-
-    private fun readContacts() {
-        Log.i("readContacts", "Reading Contacts")
-        val contentResolver = contentResolver
-        val nameCursor: Cursor? = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
-            null, null, null, null)
-        if (nameCursor!!.moveToFirst()) {
-            do {
-                val id: String = nameCursor.getString(nameCursor.getColumnIndex(ContactsContract.Contacts._ID))
-                val name: String = nameCursor.getString(nameCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                var number: String = ""
-                if (nameCursor.getInt(nameCursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0){
-                    val numberCursor: Cursor? = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", arrayOf(id), null)
-                    while (numberCursor!!.moveToNext()) {
-                        number = numberCursor.getString(numberCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                    }
-                    numberCursor.close()
-                }
-                contactList.add(Contact(name, number))
-            } while (nameCursor.moveToNext())
-        }
-        nameCursor.close()
     }
 
     companion object {
