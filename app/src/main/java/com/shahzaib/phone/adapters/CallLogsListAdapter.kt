c48aea68c45148d1e4e7c6f9494d451618631065
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2020-09-04 10:48:23 +0500

    CallLogsListAdapter.kt set up according to CallLogs Class in utilClasses.kt

--- app/src/main/java/com/shahzaib/phone/adapters/CallLogsListAdapter.kt
+++ app/src/main/java/com/shahzaib/phone/adapters/CallLogsListAdapter.kt
@@ -1,28 +1,35 @@
 package com.shahzaib.phone.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.shahzaib.phone.CallLog
+import com.shahzaib.phone.R
 
 class CallLogsListAdapter(private var list: ArrayList<CallLog>, private val context: Context) :
     RecyclerView.Adapter<CallLogsListAdapter.CustomViewHolder>() {
 
     class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        val name: TextView = view.findViewById(R.id.callLogName)
+        val description: TextView = view.findViewById(R.id.callLogDescription)
+        val photo: ImageView = view.findViewById(R.id.callLogImage)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.call_logs_list_item, parent, false)
+        return CustomViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.name.text = list[position].name
+        holder.description.text = list[position].description
+        if (list[position].photo != null)
+            holder.photo.setImageBitmap(list[position].photo)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
+    override fun getItemCount(): Int = list.size
 }
--- app/src/main/java/com/shahzaib/phone/utilClasses.kt
+++ app/src/main/java/com/shahzaib/phone/utilClasses.kt
@@ -1,5 +1,7 @@
 package com.shahzaib.phone
 
+import android.graphics.Bitmap
+
 class Contact (var name: String, var number: String)
 
-class CallLog (var name: String, var time: String)
+class CallLog (var name: String, var description: String, var photo: Bitmap?)
