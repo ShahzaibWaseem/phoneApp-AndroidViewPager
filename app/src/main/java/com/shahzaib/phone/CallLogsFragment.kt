ccf5bc9948086508668e3c2ddd51ac1a5fcd78d3
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2020-09-04 11:04:58 +0500

    CallLogsFragment.kt implemented, now merging it to master for permissions

--- app/src/main/java/com/shahzaib/phone/CallLogsFragment.kt
+++ app/src/main/java/com/shahzaib/phone/CallLogsFragment.kt
@@ -1,7 +1,10 @@
 package com.shahzaib.phone
 
 import android.content.Context
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.shahzaib.phone.adapters.CallLogsListAdapter
 import com.shahzaib.phone.databinding.CallLogsFragmentBinding
+import android.provider.CallLog.Calls
 
 class CallLogsFragment: Fragment() {
     private lateinit var binding: CallLogsFragmentBinding
@@ -36,7 +40,25 @@ class CallLogsFragment: Fragment() {
         return binding.root
     }
 
-    fun readCallLogs() {
+    private fun readCallLogs() {
+        Log.i("readContacts", "Reading Contacts")
+        val contentResolver = mContext.contentResolver
+        val callLogsUri: Uri = Uri.parse("content://call_log/calls")
+        val callLogsCursor: Cursor? = contentResolver.query(callLogsUri, null, null, null, null)
 
+        if (callLogsCursor!!.moveToFirst()) {
+            do {
+                val name: String= callLogsCursor.getString(callLogsCursor.getColumnIndex(Calls.CACHED_NAME))
+                val number: String= callLogsCursor.getString(callLogsCursor.getColumnIndex(Calls.NUMBER))
+                val duration: String = callLogsCursor.getString(callLogsCursor.getColumnIndex(Calls.DURATION))
+                val callType: Int = Integer.parseInt(callLogsCursor.getString(callLogsCursor.getColumnIndex(Calls.TYPE)))
+                var description: String = ""
+
+                description = "$number $duration $callType"
+                logsList.add(CallLog(name, description, null))
+            } while (callLogsCursor.moveToNext())
+        }
+
+        callLogsCursor.close()
     }
 }
